b7e7926e02b0dff06fd053b8e527733f01cdb941
sundaram08 <sundaramdwivedi2003@gmail.com>
2024-03-12 22:29:44 +0530

    all

---
+++ src/Components/Home/MusicBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MusicBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MusicBar
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: ['Roboto', 'Open Sans','Mathilga','San Francisco']
+  }
+});
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout/>,
+  children:[
+    { path: "",
+      element: <Home/>
+    },
+  ]
+}])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <RouterProvider router = {router}/>
+    </Provider>
+  </React.StrictMode>,
+)
